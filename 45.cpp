52d2f105bed5254ee9921aee92fc57b07aa645f0
Derrickliou1 <andyslgh@yahoo.com.tw>
2019-06-26 22:31:03 +0800

    Create 45.cpp

---
+++ 45.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+	
+
+	using namespace std;
+	
+
+	int main()
+	{
+		string s;
+		
+		getline(cin, s);
+		cout>>"There are "<<s.length()<<" characters"<<endl;
+		
+		return 0;
+	}
