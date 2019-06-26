a107bf1bdc16ac2aa863222340ec64bf58a423ee
Derrickliou1 <andyslgh@yahoo.com.tw>
2019-06-26 22:40:11 +0800

    Update 45.cpp

--- 45.cpp
+++ 45.cpp
@@ -1,16 +1,13 @@
 #include<iostream>
 #include<string>
-	
 
 	using namespace std;
 	
-
 	int main()
 	{
-		string s;
-		
+		string s;	
 		getline(cin, s);
-		cout>>"There are "<<s.length()<<" characters"<<endl;
+		cout<<"There are "<<s.length()<<" characters"<<endl;
 		
 		return 0;
 	}
